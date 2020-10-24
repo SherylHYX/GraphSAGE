0cf5048d030d12bbd2968425c30f3b5dd8ce9f4f
Yixuan He <43345664+SherylHYX@users.noreply.github.com>
2020-10-24 22:19:49 +0100

    .sh files to run

--- full_small.sh
+++ full_small.sh
@@ -25,13 +25,13 @@ python -m graphsage.unsupervised_train --train_prefix ./data/reddit/reddit --mod
 python -m graphsage.unsupervised_train --train_prefix ./data/reddit/reddit --model gcn --max_total_steps 1000 --validate_iter 10 
 python -m graphsage.unsupervised_train --train_prefix ./data/reddit/reddit --model n2v --max_total_steps 1000 --validate_iter 10 
 
-python eval_scripts/ppi_eval.py ./data/ppi unsup-ppi/graphsage_mean_big_0.000010 test
-python eval_scripts/ppi_eval.py ./data/ppi unsup-ppi/graphsage_seq_big_0.000010 test
-python eval_scripts/ppi_eval.py ./data/ppi unsup-ppi/graphsage_maxpool_big_0.000010 test
-python eval_scripts/ppi_eval.py ./data/ppi unsup-ppi/gcn_big_0.000010 test
+python eval_scripts/ppi_eval.py ./data/ppi unsup-ppi/graphsage_mean_small_0.000010 test
+python eval_scripts/ppi_eval.py ./data/ppi unsup-ppi/graphsage_seq_small_0.000010 test
+python eval_scripts/ppi_eval.py ./data/ppi unsup-ppi/graphsage_maxpool_small_0.000010 test
+python eval_scripts/ppi_eval.py ./data/ppi unsup-ppi/gcn_small_0.000010 test
 
-python eval_scripts/reddit_eval.py ./data/reddit unsup-reddit/graphsage_mean_big_0.000010 test
-python eval_scripts/reddit_eval.py ./data/reddit unsup-reddit/graphsage_seq_big_0.000010 test
-python eval_scripts/reddit_eval.py ./data/reddit unsup-reddit/graphsage_maxpool_big_0.000010 test
-python eval_scripts/reddit_eval.py ./data/reddit unsup-reddit/gcn_big_0.000010 test
-python eval_scripts/reddit_eval.py ./data/reddit unsup-reddit/n2v_big_0.000010 test
+python eval_scripts/reddit_eval.py ./data/reddit unsup-reddit/graphsage_mean_small_0.000010 test
+python eval_scripts/reddit_eval.py ./data/reddit unsup-reddit/graphsage_seq_small_0.000010 test
+python eval_scripts/reddit_eval.py ./data/reddit unsup-reddit/graphsage_maxpool_small_0.000010 test
+python eval_scripts/reddit_eval.py ./data/reddit unsup-reddit/gcn_small_0.000010 test
+python eval_scripts/reddit_eval.py ./data/reddit unsup-reddit/n2v_small_0.000010 test
