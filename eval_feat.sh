4d54c8165605502e4e358cfc6c5efbeddcc62221
Yixuan He <43345664+SherylHYX@users.noreply.github.com>
2020-10-28 08:40:28 +0000

    features only

--- eval_feat.sh
+++ eval_feat.sh
@@ -1,6 +1,2 @@
-# python eval_scripts/ppi_eval.py ./data/ppi feat test
-# python eval_scripts/ppi_eval.py ./data/ppi unsup-ppi/graphsage_mean_small_0.000010 test --embed_dir feat
-# python eval_scripts/reddit_eval.py ./data/reddit unsup-reddit/graphsage_mean_small_0.000010 test    --embed_dir feat
-# python eval_scripts/reddit_eval.py ./data/reddit feat test
-python eval_scripts/reddit_eval.py ./data/reddit n2v test
-python eval_scripts/reddit_eval.py ./data/reddit n2v feat test
+python eval_scripts/ppi_eval.py ./data/ppi feat test
+python eval_scripts/reddit_eval.py ./data/reddit feat test
